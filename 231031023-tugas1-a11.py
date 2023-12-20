cc482ac18feaddefd82621303aaebc039d5bfccb
afiqah1305 <153788238+afiqah1305@users.noreply.github.com>
2023-12-20 12:49:48 +0800

    upload tugas1 a11

---
+++ 231031023-tugas1-a11.py
@@ -0,0 +1,9 @@
+#latihan 1
+
+nilai = int(input('masukkan nilai anda'))
+batas_lulus = 65
+if nilai > batas_lulus:
+     print('Selamat,kamu lulus!')
+else:
+    print('Sayang,sekali kamu tidak lulus')
+
