1fa81d34685920b37aede45a0587b51e2c5cb746
afiqah1305 <153788238+afiqah1305@users.noreply.github.com>
2023-12-20 12:53:26 +0800

    upload tugas 3 a11

---
+++ 231031023-tugas3-a11.py
@@ -0,0 +1,9 @@
+#latihan 3
+
+nama = str(input('masukkan nama karyawan='))
+pendapatan = int(input('pendapatan='))
+
+if pendapatan > 1000 :
+    print ('status : Berhak')
+else :
+    print ('status : Tidak Berhak')
